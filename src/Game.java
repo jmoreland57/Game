804feb131fd41ad24dae067d0808a3cae52d096f
jmoreland <jmoreland157@gmail.com>
2022-08-30 13:50:58 -0700

    woo

---
+++ src/Game.java
@@ -0,0 +1,18 @@
+
+// adv topics in cs w/ theiss day 1 (or 0) example
+
+public class Game {
+	
+	private int money;
+	
+	
+	public Game () {
+		money = 100;
+	}
+
+
+	public int getMoney() {
+		return money;
+	}
+
+}
---
+++ src/Tester.java
@@ -0,0 +1,10 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		Game g = new Game();
+		System.out.println(g.getMoney());
+
+	}
+
+}
